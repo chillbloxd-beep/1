480c3618e93c6068a0d48415d579b9c7e2304564
chillbloxd-beep <chill.bloxd@gmail.com>
2026-05-29 15:18:08 +0800

    Add CLOSE_ROBLOX_CONFIG configuration object

---
+++ config.js
@@ -0,0 +1,19 @@
+export const CLOSE_ROBLOX_CONFIG = {
+  WORKER_BASE_URL: "https://brick-classroom-d1.lumjayvian.workers.dev",
+  ROOM_ID: "brick-room-001",
+  API_KEY: "ZER0PING5GWIFI$$qwerty",
+
+  // null = all connected Windows agents
+  // "nb-windows" = only NB Windows
+  TARGET_DEVICE_ID: null,
+
+  REDIRECT_URL: "https://www.google.com/",
+  MAX_WAIT_BEFORE_REDIRECT_MS: 3500,
+
+  PROCESS_NAMES: [
+    "RobloxPlayerBeta.exe",
+    "Roblox.exe",
+    "RobloxStudioBeta.exe",
+    "RobloxCrashHandler.exe"
+  ]
+};
